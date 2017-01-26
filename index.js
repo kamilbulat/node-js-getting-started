5522b2031571c0282227e2acbd5cc50cc4332d64
Kamil <kamil.bulat@interia.pl>
2017-01-26 22:16:39 +0100

    wq

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var session = require('express-session')
+var bodyParser = require('body-parser')
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -17,6 +21,20 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-app.post('/', function(request, response) {
-  response.send("ok");
+//commit
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.post('/login', function(request, response) {
+	console.log(request.body);
+  if(request.body.login == "admin" && request.body.passwd == "admin"){
+	  response.render('pages/main');
+	  request.session.user = { "login":"true" }
+	  req.session.save( function(error){} )
+  }
 });
+
+
